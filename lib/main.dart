eadf61772a2133e2947ae7ecdf8a3cedf9170677
wahyuhidayat <wahyuhidyt.56@gmail.com>
2023-05-17 22:08:21 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:sejarah_pahlawan/page/home_page.dart';
+import 'package:sejarah_pahlawan/page/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sejarah Pahlawan Indonesia',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: const Home (),
+    );
+  }
+}
